c0e1d5874cb54e3c4b9dd9877182528a7449fce6
吴强 <wuqiang@idongjia.cn>
2017-08-02 21:07:24 +0800

    订单增改查接入

--- web/src/main/java/top/jhechem/web/controller/OrderController.java
+++ web/src/main/java/top/jhechem/web/controller/OrderController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import top.jhechem.core.Response;
 import top.jhechem.core.base.Pagination;
+import top.jhechem.core.constant.ExceptionResponse;
+import top.jhechem.core.util.Assert;
 import top.jhechem.order.pojo.Order;
 import top.jhechem.order.pojo.OrderSearch;
 import top.jhechem.order.service.OrderService;
@@ -20,27 +22,46 @@ import javax.annotation.Resource;
  */
 @Controller
 @RequestMapping("order")
+@ResponseBody
 public class OrderController extends BaseController {
 
     @Resource
     private OrderService orderService;
 
     @RequestMapping(value = "test", method = RequestMethod.GET)
-    @ResponseBody
     public Response test() {
         return Response.ok();
     }
 
     @RequestMapping(value = "{bookid:\\d+}", method = RequestMethod.GET)
-    @ResponseBody
     public Response<Order> get(@PathVariable("bookid") long bookid) {
         return Response.ok(orderService.get(bookid));
     }
 
     @RequestMapping(value = "list", method = RequestMethod.GET)
-    @ResponseBody
     public Response<Pagination<Order>> list(OrderSearch search) {
 
         return Response.paginate(orderService.list(search), orderService.count(search));
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public Response add(Order order) {
+        Assert.assertNotNull(order, ExceptionResponse.MISS_ARGRUMENTS);
+        Order res = orderService.add(order);
+        return Response.ok(res);
+    }
+
+    @RequestMapping(value = "update", method = RequestMethod.PUT)
+    public Response update(Order order) {
+        Assert.assertNotNull(order, ExceptionResponse.MISS_ARGRUMENTS);
+        Assert.assertNotNull(order.getBookid(), ExceptionResponse.MISS_ARGRUMENTS);
+        int res = orderService.update(order);
+        return Response.ok(res);
+    }
+
+    @RequestMapping(value = "delete/{bookid:\\d+}", method = RequestMethod.DELETE)
+    public Response delete(@PathVariable("bookid") long bookid) {
+        int res = orderService.delete(bookid);
+        return Response.ok(res);
     }
 }
