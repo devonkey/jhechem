8c689a065cf57657bc8ec1718c89edcfe5b6ceb6
吴强 <wuqiang@idongjia.cn>
2017-09-11 23:34:24 +0800

    redis sesssion

--- web/src/main/java/top/jhechem/web/filter/ShiroLogoutFilter.java
+++ web/src/main/java/top/jhechem/web/filter/ShiroLogoutFilter.java
@@ -3,7 +3,9 @@ package top.jhechem.web.filter;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.LogoutFilter;
 import top.jhechem.core.constant.ExceptionResponse;
+import top.jhechem.user.pojo.Admin;
 import top.jhechem.web.biz.AuthBiz;
+import top.jhechem.web.shiro.SessionDAO;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletRequest;
@@ -17,11 +19,18 @@ public class ShiroLogoutFilter extends LogoutFilter {
 
     @Resource
     private AuthBiz loginBiz;
+    @Resource
+    private SessionDAO sessionDAO;
 
     @Override
     protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
         Subject subject = getSubject(request, response);
         subject.logout();
+
+        Admin admin = (Admin) subject.getPrincipal();
+        if (admin != null) {
+            sessionDAO.delByAdminId(admin.getId());
+        }
         loginBiz.redirectToLogin(request, response, ExceptionResponse.LOGOUT_SUCCESS);
         return false;
     }
