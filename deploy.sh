4c55477c634d84b8d6597ade0e2ba24ecb0ad63d
吴强 <wuqiang@idongjia.cn>
2017-09-19 10:27:19 +0800

    服务迁移到jhechem机器

--- deploy.sh
+++ deploy.sh
@@ -15,28 +15,28 @@ fi
 if [ $sendWar = true ] ; then
 	if [ $project = 'jhechem' ] ; then
         project='user'
-        scp ${project}/target/libs/${project}.war hugo@$BMW:~/war/
-        #ssh -t hugo@$BMW "sh ~/deploy.sh ${project} ${clearLog}"
+        scp ${project}/target/libs/${project}.war hugo@39.108.167.226:~/war/
+        #ssh -t hugo@39.108.167.226 "sh ~/deploy.sh ${project} ${clearLog}"
         project='order'
-        scp ${project}/target/libs/${project}.war hugo@$BMW:~/war/
-        #ssh -t hugo@$BMW "sh ~/deploy.sh ${project} ${clearLog}"
+        scp ${project}/target/libs/${project}.war hugo@39.108.167.226:~/war/
+        #ssh -t hugo@39.108.167.226 "sh ~/deploy.sh ${project} ${clearLog}"
         project='web'
-        scp ${project}/target/libs/${project}.war hugo@$BMW:~/war/
-        #ssh -t hugo@$BMW "sh ~/deploy.sh ${project} ${clearLog}"
+        scp ${project}/target/libs/${project}.war hugo@39.108.167.226:~/war/
+        #ssh -t hugo@39.108.167.226 "sh ~/deploy.sh ${project} ${clearLog}"
     else
-        scp ${project}/target/libs/${project}.war hugo@$BMW:~/war/
-        #ssh -t hugo@$BMW "sh ~/deploy.sh ${project} ${clearLog}"
+        scp ${project}/target/libs/${project}.war hugo@39.108.167.226:~/war/
+        #ssh -t hugo@39.108.167.226 "sh ~/deploy.sh ${project} ${clearLog}"
     fi
 else
     if [ $project = 'jhechem' ] ; then
         project='user'
-        ssh -t hugo@$BMW "sh ~/deploy.sh ${project} ${clearLog}"
+        ssh -t hugo@39.108.167.226 "sh ~/deploy.sh ${project} ${clearLog}"
         project='order'
-        ssh -t hugo@$BMW "sh ~/deploy.sh ${project} ${clearLog}"
+        ssh -t hugo@39.108.167.226 "sh ~/deploy.sh ${project} ${clearLog}"
         project='web'
-        ssh -t hugo@$BMW "sh ~/deploy.sh ${project} ${clearLog}"
+        ssh -t hugo@39.108.167.226 "sh ~/deploy.sh ${project} ${clearLog}"
     else
-        ssh -t hugo@$BMW "sh ~/deploy.sh ${project} ${clearLog}"
+        ssh -t hugo@39.108.167.226 "sh ~/deploy.sh ${project} ${clearLog}"
     fi
 fi
 
